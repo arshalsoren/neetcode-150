ef7f0bd3ea5e9a498b4b9c839091c5dbbe26e6fe
Arshal Soren <arshals16@yahoo.in>
2024-12-25 15:09:46 +0530

    fix: indentation

--- Two Pointers/validPalindrome.py
+++ Two Pointers/validPalindrome.py
@@ -5,15 +5,15 @@
 from typing import List
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = ''.join(e for e in s if e.isalnum()).lower()
-        return s == s[::-1]
+	def isPalindrome(self, s: str) -> bool:
+		s = ''.join(e for e in s if e.isalnum()).lower()
+		return s == s[::-1]
 
 def main():
-    s = "Was it a car or a cat I saw?"
-    solution = Solution()
-    result = solution.isPalindrome(s)
-    print(result)
+	s = "Was it a car or a cat I saw?"
+	solution = Solution()
+	result = solution.isPalindrome(s)
+	print(result)
 
 if __name__ == '__main__':
-    main()
+	main()
